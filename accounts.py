9a3065676fb9de0fd0b562cb6f5833d944fa7f68
blackCaleb <bblackcaleb@gmail.com>
2022-01-27 23:36:07 +0300

    finished accounts.py

---
+++ accounts.py
@@ -0,0 +1,43 @@
+class Accounts:
+    usr_accounts = [] #Empty user account lists
+    def __init__(self, usr_name, password):
+        self.usr_name = usr_name
+        self.password = password
+
+    def save_accounts(self):
+        '''
+        this method saves accounts into usr_accounts
+        '''
+        Accounts.usr_accounts.append(self)
+
+    def delete_accounts(self):
+        '''
+        this deletes a specific account from usr_accounts
+        '''
+        Accounts.usr_accounts.remove(self)
+
+    @classmethod
+    def find_by_usr_name(cls, usr_name):
+        '''
+        this checks if the user name already exist in usr_accounts and if it does it returns account
+        '''
+        for account in cls.usr_accounts:
+            if account.usr_name == usr_name:
+                return account
+
+    @classmethod
+    def account_exists(cls, usr_name):
+        '''
+        this seaches the usr name entered by the user in present array and returns true/false
+        '''
+        for account in cls.usr_accounts:
+            if account.usr_name == usr_name:
+                return true
+            return False
+
+    @classmethod
+    def display_accounts(cls):
+        '''
+        this displays accounts
+        '''
+        return cls.usr_accounts
